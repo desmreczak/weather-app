c1e5986338d3dad7268e4901cabe47164e70464f
Smreczak <dominik.smreczak@siemens-healthineers.com>
2018-07-18 17:38:06 -0400

    added a broken test to test event handler

--- test-app/src/App.test.js
+++ test-app/src/App.test.js
@@ -12,278 +12,318 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
 describe("\nTestStore Functions\n", () => {
 
 	it("should start with the current date", () => {
-		const store = TestStore
-		expect(store.day).toBe(new Date().getDate())
-		expect(store.month).toBe(new Date().getMonth() + 1)
-		expect(store.year).toBe(new Date().getFullYear()) 
-	})
+		const store = TestStore;
+		expect(store.day).toBe(new Date().getDate());
+		expect(store.month).toBe(new Date().getMonth() + 1);
+		expect(store.year).toBe(new Date().getFullYear());
+	});
 
 	it("should set the day", () => {
-		const store = TestStore
-		store.changeDay(12)
+		const store = TestStore;
+		store.changeDay(12);
 		expect(store.day).toBe(12)
-	})
+	});
 
 	it("should set the month", () => {
-		const store = TestStore
-		store.changeMonth(12)
+		const store = TestStore;
+		store.changeMonth(12);
 		expect(store.month).toBe(12)
-	})
+	});
 
 	it("should set the year", () => {
-		const store = TestStore
-		store.changeYear(12)
+		const store = TestStore;
+		store.changeYear(12);
 		expect(store.year).toBe(12)
-	})
+	});
 
 	it("should return temperature in Fahrenheit for a given hour(military time)", () => {
-		const store = TestStore
-		store.day = 12
-		store.month = 7
-		store.year = 2018
+		const store = TestStore;
+		store.day = 12;
+		store.month = 7;
+		store.year = 2018;
 		expect(store.returnTempForGivenHour(15)).toBe("76°F")
-	})
+	});
 
 	it("should return image url for a given hour(military time)", () => {
-		const store = TestStore
-		store.day = 12
-		store.month = 7
-		store.year = 2018
+		const store = TestStore;
+		store.day = 12;
+		store.month = 7;
+		store.year = 2018;
 		expect(store.returnWeatherForGivenHour(15)).toBe("https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_3-512.png")
-	})
+	});
 
 	it("should return formatted date", () => {
-		const store = TestStore
-		store.day = 12
-		store.month = 7
-		store.year = 2018
+		const store = TestStore;
+		store.day = 12;
+		store.month = 7;
+		store.year = 2018;
 		expect(store.returnDate).toBe("July 12th, 2018")
-	})
+	});
 
 	it("should return day", () => {
-		const store = TestStore
-		store.day = 12
-		store.month = 7
-		store.year = 2018
+		const store = TestStore;
+		store.day = 12;
+		store.month = 7;
+		store.year = 2018;
 		expect(store.returnDay).toBe(12)
-	})
+	});
 
 	it("should return month", () => {
-		const store = TestStore
-		store.day = 12
-		store.month = 7
-		store.year = 2018
+		const store = TestStore;
+		store.day = 12;
+		store.month = 7;
+		store.year = 2018;
 		expect(store.returnMonth).toBe(7)
-	})
+	});
 
 	it("should return year", () => {
-		const store = TestStore
-		store.day = 12
-		store.month = 7
-		store.year = 2018
+		const store = TestStore;
+		store.day = 12;
+		store.month = 7;
+		store.year = 2018;
 		expect(store.returnYear).toBe(2018)
 	})
-})
+});
 
 describe("\nTestStore Helper Functions\n", () => {
 
 	it("should format a given month, day, year", () => {
-		var day = 12
-		var month = 7
-		var year = 2018
+		let day = 12;
+		let month = 7;
+		let year = 2018;
 		expect(formatDate(month, day, year)).toBe("July 12th, 2018")
-	})
+	});
 
 	it("should return the name of a month for a number", () => {
 		expect(formatMonth(1)).toBe("January")
-	})
+	});
 
 	it("should return the day with the appropriate suffix", () => {
-		expect(formatDay(1)).toBe("1st")
-		expect(formatDay(2)).toBe("2nd")
-		expect(formatDay(3)).toBe("3rd")
-		expect(formatDay(4)).toBe("4rth")
-		expect(formatDay(5)).toBe("5th")
-		expect(formatDay(6)).toBe("6th")
-		expect(formatDay(7)).toBe("7th")
-		expect(formatDay(8)).toBe("8th")
-		expect(formatDay(9)).toBe("9th")
-		expect(formatDay(10)).toBe("10th")
-		expect(formatDay(11)).toBe("11th")
-		expect(formatDay(20)).toBe("20th")
-		expect(formatDay(21)).toBe("21st")
-		expect(formatDay(29)).toBe("29th")
-		expect(formatDay(30)).toBe("30th")
-	})
+		expect(formatDay(1)).toBe("1st");
+		expect(formatDay(2)).toBe("2nd");
+		expect(formatDay(3)).toBe("3rd");
+		expect(formatDay(4)).toBe("4rth");
+		expect(formatDay(5)).toBe("5th");
+		expect(formatDay(6)).toBe("6th");
+		expect(formatDay(7)).toBe("7th");
+		expect(formatDay(8)).toBe("8th");
+		expect(formatDay(9)).toBe("9th");
+		expect(formatDay(10)).toBe("10th");
+		expect(formatDay(11)).toBe("11th");
+		expect(formatDay(20)).toBe("20th");
+		expect(formatDay(21)).toBe("21st");
+		expect(formatDay(29)).toBe("29th");
+		expect(formatDay(30)).toBe("30th");
+	});
 
 	it("should return an hour from a given date string", () => {
-		var date = "2017-05-04 9:00:00"
+		let date = "2017-05-04 9:00:00";
 		expect(parseHour(date)).toBe(9)
-	})
+	});
 
 	it("should return a date from a given date string", () => {
-		var date = "2017-05-04 9:00:00"
+		let date = "2017-05-04 9:00:00";
 		expect(parseDate(date)).toEqual([4, 5, 2017])
-	})
+	});
 
 	it("should return rounded Fahrenheit value for given Kelvin", () => {
-		expect(KelvinToFahrenheit(297)).toBe(75)
-		expect(KelvinToFahrenheit(297.54)).toBe(76)
-		expect(KelvinToFahrenheit(297.3167)).toBe(76)
-		expect(KelvinToFahrenheit(297.31)).toBe(75)
-		expect(KelvinToFahrenheit(250)).toBe(-10)
-		expect(KelvinToFahrenheit(248)).toBe(-13)
-		expect(KelvinToFahrenheit(255.372)).toBe(0)
-	})
+		expect(KelvinToFahrenheit(297)).toBe(75);
+		expect(KelvinToFahrenheit(297.54)).toBe(76);
+		expect(KelvinToFahrenheit(297.3167)).toBe(76);
+		expect(KelvinToFahrenheit(297.31)).toBe(75);
+		expect(KelvinToFahrenheit(250)).toBe(-10);
+		expect(KelvinToFahrenheit(248)).toBe(-13);
+		expect(KelvinToFahrenheit(255.372)).toBe(0);
+	});
 
 	it("should return a url from a given weather and array of icon urls", () => {
-		const store = TestStore
-		var urls = store.weatherIcons
-		expect(getIconURL("Clear", urls)).toBe("https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_3-512.png")
+		const store = TestStore;
+		let urls = store.weatherIcons;
+		expect(getIconURL("Clear", urls)).toBe("https://cdn4.iconfinder.com/data/icons/the-weather-is-nice-today/64/weather_3-512.png");
 		expect(getIconURL("", urls)).toBe("")
 	})
 
-})
+});
 
 describe("\nApp Component\n", () => {
 
-	const store = TestStore
-	const shallowWrap = shallow(<App TestStore={store} />)
-	const renderWrap = render(<App TestStore={store} />)
-	const mountWrap = mount(<App TestStore={store} />)
+	const store = TestStore;
+	const shallowWrap = shallow(<App TestStore={store} />);
+	const renderWrap = render(<App TestStore={store} />);
+	const mountWrap = mount(<App TestStore={store} />);
 
 	it("should render App component", () => {
 		expect(shallowWrap).toHaveLength(1);
-	})
+	});
 
 	it("should render the date", () => {
 		expect(renderWrap.find(".app").length).toBe(1);
-	})
+	});
 
 	it("should render 5 weather containers", () => {
 		expect(renderWrap.find(".weather").length).toBe(5);
-	})
+	});
 
 	it("should respond to button click and increase the month (no wrapping)", () => {
-		store.month = 1
-		var month = mountWrap.find('.app').text().split(" ")[0]
-		var monthsList = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-		var expectedMonth = monthsList[store.month + 1]
+		store.month = 1;
+
+		let monthsList = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+		let expectedMonth = monthsList[store.month + 1];
 
 		mountWrap.find('#month-up').simulate('click');
 
-		expect(mountWrap.find('.app').text().split(" ")[0]).toBe(expectedMonth);
-	})
+        let month = mountWrap.find('.app').text().split(" ")[0];
+
+		expect(month).toBe(expectedMonth);
+	});
 
 	it("should respond to button click and increase the month (wrap around)", () => {
-		store.month = 12
-		var month = mountWrap.find('.app').text().split(" ")[0]
-		var expectedMonth = "January"
+		store.month = 12;
+
+		let expectedMonth = "January";
 
 		mountWrap.find('#month-up').simulate('click');
 
-		expect(mountWrap.find('.app').text().split(" ")[0]).toBe(expectedMonth);
-	})
+        let month = mountWrap.find('.app').text().split(" ")[0];
+
+		expect(month).toBe(expectedMonth);
+	});
 
 	it("should respond to button click and decrease the month (no wrapping)", () => {
-		store.month = 12
-		var month = mountWrap.find('.app').text().split(" ")[0]
-		var monthsList = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-		var expectedMonth = monthsList[store.month - 1]
+		store.month = 12;
+
+		let monthsList = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+		let expectedMonth = monthsList[store.month - 1];
 
 		mountWrap.find('#month-down').simulate('click');
 
-		expect(mountWrap.find('.app').text().split(" ")[0]).toBe(expectedMonth);
-	})
+        let month = mountWrap.find('.app').text().split(" ")[0];
+
+		expect(month).toBe(expectedMonth);
+	});
 
 	it("should respond to button click and decrease the month (wrap around)", () => {
-		store.month = 1
-		var month = mountWrap.find('.app').text().split(" ")[0]
-		var expectedMonth = "December"
+		store.month = 1;
+
+		let expectedMonth = "December";
 
 		mountWrap.find('#month-down').simulate('click');
 
-		expect(mountWrap.find('.app').text().split(" ")[0]).toBe(expectedMonth);
-	})
+        let month = mountWrap.find('.app').text().split(" ")[0];
+
+		expect(month).toBe(expectedMonth);
+	});
 
 	it("should respond to button click and increase the day (no wrapping)", () => {
-		store.day = 1
-		var day = parseInt(mountWrap.find('.app').text().split(/[\s|(th)|(st)|(nd)|(rd)|(rth)]+/)[1])
-		var expectedDay = store.day + 1
+		store.day = 1;
+
+		let expectedDay = store.day + 1;
 
 		mountWrap.find('#day-up').simulate('click');
 
-		expect(parseInt(mountWrap.find('.app').text().split(/[\s|(th)|(st)|(nd)|(rd)|(rth)]+/)[1])).toBe(expectedDay);
-	})
+        let day = parseInt(mountWrap.find('.app').text().split(/[\s|(th)|(st)|(nd)|(rd)|(rth)]+/)[1]);
+
+		expect(day).toBe(expectedDay);
+	});
 
 	it("should respond to button click and increase the day (wrap around)", () => {
-		store.day = 31
-		var day = parseInt(mountWrap.find('.app').text().split(/[\s|(th)|(st)|(nd)|(rd)|(rth)]+/)[1])
-		var expectedDay = 1
+		store.day = 31;
+
+		let expectedDay = 1;
 
 		mountWrap.find('#day-up').simulate('click');
 
-		expect(parseInt(mountWrap.find('.app').text().split(/[\s|(th)|(st)|(nd)|(rd)|(rth)]+/)[1])).toBe(expectedDay);
-	})
+        let day = parseInt(mountWrap.find('.app').text().split(/[\s|(th)|(st)|(nd)|(rd)|(rth)]+/)[1]);
+
+		expect(day).toBe(expectedDay);
+	});
 
 	it("should respond to button click and decrease the day (no wrapping)", () => {
-		store.day = 31
-		var day = parseInt(mountWrap.find('.app').text().split(/[\s|(th)|(st)|(nd)|(rd)|(rth)]+/)[1])
-		var expectedDay = store.day - 1
+		store.day = 31;
+
+		let expectedDay = store.day - 1;
 	
 		mountWrap.find('#day-down').simulate('click');
 
-		expect(parseInt(mountWrap.find('.app').text().split(/[\s|(th)|(st)|(nd)|(rd)|(rth)]+/)[1])).toBe(expectedDay);
-	})
+        let day = parseInt(mountWrap.find('.app').text().split(/[\s|(th)|(st)|(nd)|(rd)|(rth)]+/)[1]);
+
+		expect(day).toBe(expectedDay);
+	});
 
 	it("should respond to button click and decrease the day (wrap around)", () => {
-		store.day = 1
-		var day = parseInt(mountWrap.find('.app').text().split(/[\s|(th)|(st)|(nd)|(rd)|(rth)]+/)[1])
-		var expectedDay = 31
+		store.day = 1;
+
+		let expectedDay = 31;
 
 		mountWrap.find('#day-down').simulate('click');
 
-		expect(parseInt(mountWrap.find('.app').text().split(/[\s|(th)|(st)|(nd)|(rd)|(rth)]+/)[1])).toBe(expectedDay);
-	})
+        let day = parseInt(mountWrap.find('.app').text().split(/[\s|(th)|(st)|(nd)|(rd)|(rth)]+/)[1]);
+
+		expect(day).toBe(expectedDay);
+	});
 
 	it("should respond to button click and increase the year (no wrapping)", () => {
-		store.year = 2000
-		var year = mountWrap.find('.app').text().split(" ").map(Number).filter(Boolean)[0]
-		var expectedYear = store.year + 1
+		store.year = 2000;
+
+		let expectedYear = store.year + 1;
 
 		mountWrap.find('#year-up').simulate('click');
 
-		expect(mountWrap.find('.app').text().split(" ").map(Number).filter(Boolean)[0]).toBe(expectedYear);
-	})
+        let year = mountWrap.find('.app').text().split(" ").map(Number).filter(Boolean)[0];
+
+		expect(year).toBe(expectedYear);
+	});
 
 	it("should respond to button click and increase the year (wrap around)", () => {
-		store.year = 2020
-		var year = mountWrap.find('.app').text().split(" ").map(Number).filter(Boolean)[0]
-		var expectedYear = 2000;
+		store.year = 2020;
+
+		let expectedYear = 2000;
 
 		mountWrap.find('#year-up').simulate('click');
 
-		expect(mountWrap.find('.app').text().split(" ").map(Number).filter(Boolean)[0]).toBe(expectedYear);
-	})
+        let year = mountWrap.find('.app').text().split(" ").map(Number).filter(Boolean)[0];
+
+        expect(year).toBe(expectedYear);
+	});
 
 	it("should respond to button click and decrease the year (no wrapping)", () => {
-		store.year = 2020
-		var year = mountWrap.find('.app').text().split(" ").map(Number).filter(Boolean)[0]
-		var expectedYear = store.year - 1
+		store.year = 2020;
+
+		let expectedYear = store.year - 1;
 
 		mountWrap.find('#year-down').simulate('click');
 
-		expect(mountWrap.find('.app').text().split(" ").map(Number).filter(Boolean)[0]).toBe(expectedYear);
-	})
+        let year = mountWrap.find('.app').text().split(" ").map(Number).filter(Boolean)[0];
+
+        expect(year).toBe(expectedYear);
+	});
 
 	it("should respond to button click and decrease the year (wrap around)", () => {
-		store.year = 2000
-		var year = mountWrap.find('.app').text().split(" ").map(Number).filter(Boolean)[0]
-		var expectedYear = 2020;
+		store.year = 2000;
+		let expectedYear = 2020;
 
 		mountWrap.find('#year-down').simulate('click');
 
-		expect(mountWrap.find('.app').text().split(" ").map(Number).filter(Boolean)[0]).toBe(expectedYear);
+        let year = mountWrap.find('.app').text().split(" ").map(Number).filter(Boolean)[0];
+
+        expect(year).toBe(expectedYear);
+	});
+
+	it("should throw error if incorrect parameter for handChangeDate() is given", () => {
+
+		setFixtures('<div id="testContainer" onClick=handleChangedate(true, "test")></div>');
+
+		let spyEvent = spyOnEvent('#testContainer', 'click');
+
+		store.year = 2000;
+
+		let expectedYear = 2020;
+
+		mountWrap.find('#year-down').simulate('click');
+
+        let year = mountWrap.find('.app').text().split(" ").map(Number).filter(Boolean)[0];
+
+		expect(year).toBe(expectedYear);
 	})
 
-})
+});
--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -87,43 +87,43 @@ class App extends Component<{TestStore: any}, {}> {
     const {TestStore} = this.props;
 
     return (
-      <div className="background">
-      <div id="top-arrow-container">
-      <i id="month-up" className="up" onClick={e => this.handleChangeDate(true, "month")}></i>
-      <i id="day-up" className="up" onClick={e => this.handleChangeDate(true, "day")}></i>
-      <i id="year-up" className="up" onClick={e => this.handleChangeDate(true, "year")}></i>
-      </div>
-      <div className="app">
-      {TestStore.returnDate}
-      </div>
-      <div id="bot-arrow-container">
-      <i id="month-down" className="down" onClick={e => this.handleChangeDate(false, "month")}></i>
-      <i id="day-down" className="down" onClick={e => this.handleChangeDate(false, "day")}></i>
-      <i id="year-down" className="down" onClick={e => this.handleChangeDate(false, "year")}></i>
-      </div>
-      <div id="container">
-      <div id="div1" className="weather">
-      <div className="time">
-      <div className="child">
-      9 am
-      </div>
-      </div>
-      <div className="icon">
-      <img src={TestStore.returnWeatherForGivenHour(9)} />
-      </div>
-      <div className="degrees">
-      <div className="child">
-      {TestStore.returnTempForGivenHour(9)}
-      </div>
-      </div>
-      </div>
-      <div id="div2" className="weather"><div className="time"><div className="child">12 pm</div></div><div className="icon"><img src={TestStore.returnWeatherForGivenHour(12)} /></div><div className="degrees"><div className="child">{TestStore.returnTempForGivenHour(12)}</div></div></div>
-      <div id="div3" className="weather"><div className="time"><div className="child">3 pm</div></div><div className="icon"><img src={TestStore.returnWeatherForGivenHour(15)} /></div><div className="degrees"><div className="child">{TestStore.returnTempForGivenHour(15)}</div></div></div>
-      <div id="div4" className="weather"><div className="time"><div className="child">6 pm</div></div><div className="icon"><img src={TestStore.returnWeatherForGivenHour(18)} /></div><div className="degrees"><div className="child">{TestStore.returnTempForGivenHour(18)}</div></div></div>
-      <div id="div5" className="weather"><div className="time"><div className="child">9 pm</div></div><div className="icon"><img src={TestStore.returnWeatherForGivenHour(21)} /></div><div className="degrees"><div className="child">{TestStore.returnTempForGivenHour(21)}</div></div></div>
-      </div>
-      </div>      
-      );
+        <div className="background">
+          <div id="top-arrow-container">
+            <i id="month-up" className="up" onClick={e => this.handleChangeDate(true, "month")}></i>
+            <i id="day-up" className="up" onClick={e => this.handleChangeDate(true, "day")}></i>
+            <i id="year-up" className="up" onClick={e => this.handleChangeDate(true, "year")}></i>
+          </div>
+          <div className="app">
+            {TestStore.returnDate}
+          </div>
+          <div id="bot-arrow-container">
+            <i id="month-down" className="down" onClick={e => this.handleChangeDate(false, "month")}></i>
+            <i id="day-down" className="down" onClick={e => this.handleChangeDate(false, "day")}></i>
+            <i id="year-down" className="down" onClick={e => this.handleChangeDate(false, "year")}></i>
+          </div>
+          <div id="container">
+            <div id="div1" className="weather">
+              <div className="time">
+                <div className="child">
+                  9 am
+                </div>
+              </div>
+              <div className="icon">
+                <img src={TestStore.returnWeatherForGivenHour(9)} />
+              </div>
+              <div className="degrees">
+                <div className="child">
+                  {TestStore.returnTempForGivenHour(9)}
+                </div>
+              </div>
+            </div>
+            <div id="div2" className="weather"><div className="time"><div className="child">12 pm</div></div><div className="icon"><img src={TestStore.returnWeatherForGivenHour(12)} /></div><div className="degrees"><div className="child">{TestStore.returnTempForGivenHour(12)}</div></div></div>
+            <div id="div3" className="weather"><div className="time"><div className="child">3 pm</div></div><div className="icon"><img src={TestStore.returnWeatherForGivenHour(15)} /></div><div className="degrees"><div className="child">{TestStore.returnTempForGivenHour(15)}</div></div></div>
+            <div id="div4" className="weather"><div className="time"><div className="child">6 pm</div></div><div className="icon"><img src={TestStore.returnWeatherForGivenHour(18)} /></div><div className="degrees"><div className="child">{TestStore.returnTempForGivenHour(18)}</div></div></div>
+            <div id="div5" className="weather"><div className="time"><div className="child">9 pm</div></div><div className="icon"><img src={TestStore.returnWeatherForGivenHour(21)} /></div><div className="degrees"><div className="child">{TestStore.returnTempForGivenHour(21)}</div></div></div>
+          </div>
+        </div>
+    );
 
   }
 
